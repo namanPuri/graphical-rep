8c803011721255e5feb7c51981bd1918348338ba
namanpuri <namanpuri1712@gmail.com>
2022-03-06 00:04:26 +0530

    second

---
+++ readme.md
@@ -0,0 +1,1 @@
+![Alt text](/chart.png?raw=true "Title")
--- test1.py
+++ test1.py
@@ -3,21 +3,21 @@
 import matplotlib.pyplot as plt
  
 # x axis values
-x = [1,2,3]
+x = [1,2,3,4]
 # corresponding y axis values
-y = [2,4,1]
+y = [2,4,1,4]
  
 # plotting the points
 plt.plot(x, y, marker='o', markerfacecolor='black', markersize=5)
 plt.plot(x, y)
  
 # naming the x axis
-plt.xlabel('x - axis')
+plt.xlabel('Data')
 # naming the y axis
-plt.ylabel('y - axis')
+plt.ylabel('Temperature')
  
 # giving a title to my graph
-plt.title('My first graph!')
-plt.savefig('books_read.png')
+plt.title('Weather Station Data!')
+plt.savefig('chart.png')
 # function to show the plot
 plt.show()
